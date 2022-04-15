55a228997b4353062d780eee33935e17fa251616
Zander <zander@zanderf.net>
2022-04-15 15:42:21 +0000

    feat: octree and helper utils

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import read from './read';
 import write from './write';
 import * as utils from './utils';
 import * as constants from './constants';
+import * as helper from './helper';
+export * from './octree';
 export * from './types';
 
 // https://i.imgur.com/cv1fDWs.png
-const brs = { read, write, utils, constants };
-export { read, write, utils, constants };
+const brs = { read, write, utils, constants, helper };
+export { read, write, utils, constants, helper };
 export default brs;
 
 declare global {
