5bc948046fcbce392edd4494cbad17f9ba861ffe
Isaac Hirschfeld <gpg@reheatedcake.io>
2022-04-18 23:58:29 -0500

    remove unnecesary window type export

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,6 @@ const brs = { read, write, utils, constants };
 export { read, write, utils, constants };
 export default brs;
 
-declare global {
-  interface Window {
-    BRS: {
-      read: typeof read;
-      write: typeof write;
-      utils: typeof utils;
-      constants: typeof constants;
-    };
-  }
-}
-
 if (typeof window !== 'undefined') {
   window.BRS = brs;
 }
